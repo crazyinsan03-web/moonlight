130750140285fa6ade44a14a19b954fb2477a04f
crazyinsan03-web <crazyinsan03@gmail.com>
2026-03-31 14:19:19 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template, request
+import yt_dlp
+
+app = Flask(__name__)
+
+def get_song_info(query):
+    ydl_opts = {
+        'format': 'bestaudio/best',
+        'noplaylist': True,
+        'quiet': True,
+        'default_search': 'ytsearch1'
+    }
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(f"ytsearch:{query}", download=False)
+        if 'entries' in info and len(info['entries']) > 0:
+            res = info['entries'][0]
+            return {
+                'title': res.get('title'),
+                'thumbnail': res.get('thumbnail'),
+                'audio_url': res.get('url')
+            }
+    return None
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    song = None
+    if request.method == 'POST':
+        query = request.form.get('song_name')
+        if query:
+            song = get_song_info(query)
+    return render_template('index.html', song=song)
+
+if __name__ == "__main__":
+    app.run(debug=True)
