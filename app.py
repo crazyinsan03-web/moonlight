3de206ca32519178d05fba9d3d76f03a40b8b675
crazyinsan03-web <crazyinsan03@gmail.com>
2026-03-31 15:13:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,15 @@ import yt_dlp
 app = Flask(__name__)
 
 def get_song_info(query):
-    ydl_opts = {
-        'format': 'bestaudio/best',
-        'noplaylist': True,
-        'quiet': True,
-        'default_search': 'ytsearch1'
-    }
+   ydl_opts = {
+    'format': 'bestaudio/best',
+    'noplaylist': True,
+    'cookiefile': 'youtube.com_cookies.txt', # YE LINE ADD KARNI HAI
+    'quiet': True,
+    'no_warnings': True,
+    'default_search': 'ytsearch',
+    'user_agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
+}
     with yt_dlp.YoutubeDL(ydl_opts) as ydl:
         info = ydl.extract_info(f"ytsearch:{query}", download=False)
         if 'entries' in info and len(info['entries']) > 0:
